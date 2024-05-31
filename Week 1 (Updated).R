0e63b210c91d2b096aadc3abead3476a60b6cd54
tclark23 <tclark23@nd.edu>
2024-05-31 14:01:03 -0400

    Update Week 1 (Updated).R

--- Week 1 (Updated).R
+++ Week 1 (Updated).R
@@ -73,3 +73,88 @@ colnames(new_final_df) <- c("DARI 1", "DARI 2")
 ICC(new_final_df, lmer = FALSE)
 
 # This gives us the same result with a lot fewer steps.
+
+################################################################################
+# Alright, now let's assume that we already have the data in the correct format
+# (one row per subject, two columns for variables). Let's write a function that
+# takes such a data frame and produces a value for the coefficient of variation.
+
+# First, let's make a function that does everything that we did before to 
+# transform our dataframe into something we can use.
+
+df_inter_transform <- function(df, session, variable) {
+  new_subset <- df %>%
+    filter(`Session ID` == session) %>%
+    select(`DARI ID`, `Subject ID`, all_of(variable))
+  
+  new_final_df <- new_subset %>%
+    pivot_wider(names_from = `DARI ID`, values_from = variable) %>%
+    select("1", "2")
+  
+  colnames(new_final_df) <- c("DARI 1", "DARI 2")
+  
+  return(new_final_df)
+}
+
+
+# example of how we would use this:
+df_inter_transform(DataSubjects21, "1", "Shoulder External Rotation Max Left")
+
+
+cv_inter <- function(df, session, variable) {
+  new_df <- df_inter_transform(df, session, variable)
+  
+  coefs <- c()
+  for (i in 1:nrow(new_df)) {
+    sdev <- sd(as.numeric(new_df[i, ]))
+    avg <- mean(as.numeric(new_df[i, ]))
+    cov <- sdev*100/avg
+    coefs <- append(coefs, cov)
+  }
+  return(mean(coefs))
+}
+
+cv_inter(DataSubjects21, "1", "Shoulder External Rotation Max Left")
+
+# Ok, now we do the same thing as above, but we reformat the data for an
+# INTRA-rater reliability study instead.
+df_intra_transform <- function(df, DARI, variable) {
+  new_subset <- df %>%
+    filter(`DARI ID` == DARI) %>%
+    select(`Session ID`, `Subject ID`, all_of(variable))
+  
+  new_final_df <- new_subset %>%
+    pivot_wider(names_from = `Session ID`, values_from = variable) %>%
+    select("1", "2")
+  
+  colnames(new_final_df) <- c("Session 1", "Session 2")
+  
+  return(new_final_df)
+}
+df_intra_transform(DataSubjects21, "2", "Shoulder External Rotation Max Left")
+
+cv_intra <- function(df, DARI, variable) {
+  new_df <- df_intra_transform(df, DARI, variable)
+  
+  coefs <- c()
+  for (i in 1:nrow(new_df)) {
+    sdev <- sd(as.numeric(new_df[i, ]))
+    avg <- mean(as.numeric(new_df[i, ]))
+    cov <- sdev*100/avg
+    coefs <- append(coefs, cov)
+  }
+  return(mean(coefs))
+}
+
+cv_intra(DataSubjects21, "2", "Shoulder External Rotation Max Left")
+
+general_cv <- function(df, type, value, variable) {
+  if (type == "inter") {
+    return(cv_inter(df, value, variable))
+  }
+  else if (type == "intra") {
+    return(cv_intra(df, value, variable))
+  }
+}
+
+general_cv(DataSubjects21, "inter", "1", "Shoulder External Rotation Max Left")
